2ce9279de78818a8c13c9cc1d786da907515229a
cwlroda <weiloon.c97@gmail.com>
2022-05-26 00:16:24 +0200

    Pytorch 1.10.2 port for detectron2 compatibility

--- pytorch_plugin/overwrite.sh
+++ pytorch_plugin/overwrite.sh
@@ -1,6 +1,7 @@
-PYTORCH_PATH=$1
+VERSION=$1
+PYTORCH_PATH=$2
 
-cp __init__.py $PYTORCH_PATH/torch/cuda/__init__.py
-cp Module.cpp $PYTORCH_PATH/torch/csrc/cuda/Module.cpp
-cp CUDACachingAllocator.h $PYTORCH_PATH/c10/cuda/CUDACachingAllocator.h
-cp CUDACachingAllocator.cpp $PYTORCH_PATH/c10/cuda/CUDACachingAllocator.cpp
+cp $VERSION/__init__.py $PYTORCH_PATH/torch/cuda/__init__.py
+cp $VERSION/Module.cpp $PYTORCH_PATH/torch/csrc/cuda/Module.cpp
+cp $VERSION/CUDACachingAllocator.h $PYTORCH_PATH/c10/cuda/CUDACachingAllocator.h
+cp $VERSION/CUDACachingAllocator.cpp $PYTORCH_PATH/c10/cuda/CUDACachingAllocator.cpp
